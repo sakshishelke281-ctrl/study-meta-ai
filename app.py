ca23f7bea1dfbd325f1f9ab215335303b77090d2
Sakshi Shelke <sakshishelke281@gmail.com>
2026-09-06 21:50:04 +0530

    Connect Render PostgreSQL database

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ app.secret_key = os.getenv("SECRET_KEY", "study-meta-ai-secret")
 # ==============================
 
 def get_db_connection():
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
     return psycopg2.connect(
         host="localhost",
         database="study meta ai",
@@ -21,7 +26,6 @@ def get_db_connection():
         password=os.getenv("DB_PASSWORD", "55555"),
         port="5432"
     )
-
 
 # ==============================
 # HOME
